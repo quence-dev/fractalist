34101a86273e580a32f12c0b3fca7c981fff30d9
Spencer Vilicic <quence@bu.edu>
2021-05-04 01:41:27 -0400

    test playlist API call

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -16,13 +16,6 @@ fetch('https://api.spotify.com/v1/me/playlists', {
     'Authorization': 'Bearer ' + code
   }
 })
-  .then((res) => res.json())
-  .then((data) => {
-    var playlists = data
-    console.log(data)
-  });
-
-fetch('resources/mock.json')
   .then(response => response.json())
   .then(data => {
     console.log(data);
@@ -36,7 +29,7 @@ fetch('resources/mock.json')
 
       //set classes
       card.setAttribute("class", "card");
-      grid.setAttribute("class","card-grid");
+      grid.setAttribute("class", "card-grid");
       h3.setAttribute("class", "card-title");
       h4.setAttribute("class", "card-owner");
       img.setAttribute("class", "card-img");
@@ -63,3 +56,45 @@ fetch('resources/mock.json')
     }
   })
   .catch(console.error);
+
+  // fetch('resources/mock.json')
+  // .then(response => response.json())
+  // .then(data => {
+  //   console.log(data);
+  //   for (const playlist of data.items) {
+  //     //create HTML elements
+  //     const card = document.createElement("div");
+  //     const grid = document.createElement("div");
+  //     const h3 = document.createElement("h3");
+  //     const h4 = document.createElement("h4");
+  //     const img = document.createElement("img");
+
+  //     //set classes
+  //     card.setAttribute("class", "card");
+  //     grid.setAttribute("class","card-grid");
+  //     h3.setAttribute("class", "card-title");
+  //     h4.setAttribute("class", "card-owner");
+  //     img.setAttribute("class", "card-img");
+
+  //     //set text and images
+  //     h3.textContent = `${playlist.name}`;
+  //     h4.textContent = `by ${playlist.owner.display_name}`;
+  //     img.src = playlist.images[0].url;
+
+  //     //append elements to page
+  //     container.appendChild(card);
+  //     card.appendChild(img);
+  //     card.appendChild(grid);
+  //     grid.appendChild(h3);
+  //     grid.appendChild(h4);
+
+  //     //event for clicking card
+  //     card.addEventListener("click", () => {
+  //       const a = document.createElement("a");
+  //       a.href = "generator_mock.html";
+  //       a.click();
+  //       document.body.removeChild(a);
+  //     })
+  //   }
+  // })
+  // .catch(console.error);
