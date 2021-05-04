8fbde300c65c85cec751b40dcc2c253e6f1fad5f
Spencer Vilicic <quence@bu.edu>
2021-05-04 02:50:25 -0400

    checking fetch

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -7,7 +7,7 @@ app.appendChild(container);
 const loadBtn = document.createElement("button");
 loadBtn.setAttribute("class", "myButton");
 loadBtn.appendChild(document.createTextNode('Load More'));
-loadBtn.addEventListener("click", load);
+loadBtn.addEventListener("click", fetch);
 document.body.appendChild(loadBtn);
 
 //here is list of playlists
@@ -15,67 +15,64 @@ const params = new URLSearchParams(window.location.search);
 var code = (params.get("code"));
 let endpoint = 'https://api.spotify.com/v1/me/playlists'; /* initial endpoint */
 
-function load() {
-  fetch(endpoint, {
-    headers:
-    {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + code
+fetch(endpoint, {
+  headers:
+  {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + code
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+
+    for (const playlist of data.items) {
+      //create HTML elements
+      const card = document.createElement("div");
+      const grid = document.createElement("div");
+      const h3 = document.createElement("h3");
+      const h4 = document.createElement("h4");
+      const img = document.createElement("img");
+
+      //set classes
+      card.setAttribute("class", "card");
+      grid.setAttribute("class", "card-grid");
+      h3.setAttribute("class", "card-title");
+      h4.setAttribute("class", "card-owner");
+      img.setAttribute("class", "card-img");
+
+      //set text and images
+      h3.textContent = `${playlist.name}`;
+      h4.textContent = `by ${playlist.owner.display_name}`;
+      img.src = playlist.images[0].url;
+
+      //append elements to page
+      container.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(grid);
+      grid.appendChild(h3);
+      grid.appendChild(h4);
+
+      //event for clicking card
+      card.addEventListener("click", () => {
+        const a = document.createElement("a");
+        a.href = "generator_mock.html";
+        a.click();
+        document.body.removeChild(a);
+      })
     }
   })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      
-      for (const playlist of data.items) {
-        //create HTML elements
-        const card = document.createElement("div");
-        const grid = document.createElement("div");
-        const h3 = document.createElement("h3");
-        const h4 = document.createElement("h4");
-        const img = document.createElement("img");
+  .then(data => { //must be added to async chain
+    //Remove load more button if there's no more playlists
+    if (data.next !== null) {
+      endpoint = data.next;
+    } else {
+      document.body.removeChild(loadBtn);
+    }
+  })
+  .catch(console.error);
 
-        //set classes
-        card.setAttribute("class", "card");
-        grid.setAttribute("class", "card-grid");
-        h3.setAttribute("class", "card-title");
-        h4.setAttribute("class", "card-owner");
-        img.setAttribute("class", "card-img");
-
-        //set text and images
-        h3.textContent = `${playlist.name}`;
-        h4.textContent = `by ${playlist.owner.display_name}`;
-        img.src = playlist.images[0].url;
-
-        //append elements to page
-        container.appendChild(card);
-        card.appendChild(img);
-        card.appendChild(grid);
-        grid.appendChild(h3);
-        grid.appendChild(h4);
-
-        //event for clicking card
-        card.addEventListener("click", () => {
-          const a = document.createElement("a");
-          a.href = "generator_mock.html";
-          a.click();
-          document.body.removeChild(a);
-        })
-      }
-    })
-    .then(data => { //must be added to async chain
-      //Remove load more button if there's no more playlists
-      if (data.next !== null) {
-        endpoint = data.next;
-      } else {
-        document.body.removeChild(loadBtn);
-      }
-    })
-    .catch(console.error);
-}
-
-load();
 
   // fetch('resources/mock.json')
   // .then(response => response.json())
