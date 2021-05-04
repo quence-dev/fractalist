b607b5912d4238115a91d4dc39a9b19b423d45ca
Spencer Vilicic <quence@bu.edu>
2021-05-04 02:13:48 -0400

    configure "load more" for playlists

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -3,11 +3,12 @@ const app = document.getElementById("root");
 const container = document.createElement("div");
 container.setAttribute("class", "container");
 app.appendChild(container);
+let next = "";
 
 //here is list of playlists
 const params = new URLSearchParams(window.location.search);
 var code = (params.get("code"));
-console.log(code);
+
 fetch('https://api.spotify.com/v1/me/playlists', {
   headers:
   {
@@ -19,6 +20,8 @@ fetch('https://api.spotify.com/v1/me/playlists', {
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    next = data.next;
+
     for (const playlist of data.items) {
       //create HTML elements
       const card = document.createElement("div");
@@ -56,6 +59,59 @@ fetch('https://api.spotify.com/v1/me/playlists', {
     }
   })
   .catch(console.error);
+
+  function loadMore() {
+    fetch(next, {
+  headers:
+  {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + code
+  }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    next = data.next;
+
+    for (const playlist of data.items) {
+      //create HTML elements
+      const card = document.createElement("div");
+      const grid = document.createElement("div");
+      const h3 = document.createElement("h3");
+      const h4 = document.createElement("h4");
+      const img = document.createElement("img");
+
+      //set classes
+      card.setAttribute("class", "card");
+      grid.setAttribute("class", "card-grid");
+      h3.setAttribute("class", "card-title");
+      h4.setAttribute("class", "card-owner");
+      img.setAttribute("class", "card-img");
+
+      //set text and images
+      h3.textContent = `${playlist.name}`;
+      h4.textContent = `by ${playlist.owner.display_name}`;
+      img.src = playlist.images[0].url;
+
+      //append elements to page
+      container.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(grid);
+      grid.appendChild(h3);
+      grid.appendChild(h4);
+
+      //event for clicking card
+      card.addEventListener("click", () => {
+        const a = document.createElement("a");
+        a.href = "generator_mock.html";
+        a.click();
+        document.body.removeChild(a);
+      })
+    }
+  })
+  .catch(console.error);
+  }
 
   // fetch('resources/mock.json')
   // .then(response => response.json())
