91270f0c1f2601b0030e31360c309b5ac791dfbe
Spencer Vilicic <quence@bu.edu>
2021-05-04 03:06:24 -0400

    reverting changes to separate fetch functions

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -4,11 +4,7 @@ const container = document.createElement("div");
 container.setAttribute("class", "container");
 app.appendChild(container);
 
-const loadBtn = document.createElement("button");
-loadBtn.setAttribute("class", "myButton");
-loadBtn.appendChild(document.createTextNode('Load More'));
-loadBtn.addEventListener("click", fetch);
-document.body.appendChild(loadBtn);
+const loadBtn = document.getElementById("loadBtn");
 
 //here is list of playlists
 const params = new URLSearchParams(window.location.search);
@@ -26,6 +22,7 @@ fetch(endpoint, {
   .then(response => response.json())
   .then(data => {
     console.log(data);
+    endpoint = data.next;
 
     for (const playlist of data.items) {
       //create HTML elements
@@ -63,15 +60,63 @@ fetch(endpoint, {
       })
     }
   })
-  .then(data => { //must be added to async chain
-    //Remove load more button if there's no more playlists
-    if (data.next !== null) {
-      endpoint = data.next;
-    } else {
-      document.body.removeChild(loadBtn);
+  .catch(console.error);
+
+function load() {
+  fetch(endpoint, {
+    headers:
+    {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + code
     }
   })
-  .catch(console.error);
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      endpoint = data.next;
+      // if (endpoint == null) {
+      //   loadBtn.setAttribute("visibility", "none");
+      // }
+
+      for (const playlist of data.items) {
+        //create HTML elements
+        const card = document.createElement("div");
+        const grid = document.createElement("div");
+        const h3 = document.createElement("h3");
+        const h4 = document.createElement("h4");
+        const img = document.createElement("img");
+
+        //set classes
+        card.setAttribute("class", "card");
+        grid.setAttribute("class", "card-grid");
+        h3.setAttribute("class", "card-title");
+        h4.setAttribute("class", "card-owner");
+        img.setAttribute("class", "card-img");
+
+        //set text and images
+        h3.textContent = `${playlist.name}`;
+        h4.textContent = `by ${playlist.owner.display_name}`;
+        img.src = playlist.images[0].url;
+
+        //append elements to page
+        container.appendChild(card);
+        card.appendChild(img);
+        card.appendChild(grid);
+        grid.appendChild(h3);
+        grid.appendChild(h4);
+
+        //event for clicking card
+        card.addEventListener("click", () => {
+          const a = document.createElement("a");
+          a.href = "generator_mock.html";
+          a.click();
+          document.body.removeChild(a);
+        })
+      }
+    })
+    .catch(console.error);
+}
 
 
   // fetch('resources/mock.json')
