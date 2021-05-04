f145d55548668135a5388dc2f6ad1905b97ca595
Spencer Vilicic <quence@bu.edu>
2021-05-04 03:53:21 -0400

    more debugging of playlist

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -10,6 +10,7 @@ const loadBtn = document.getElementById("loadBtn");
 const params = new URLSearchParams(window.location.search);
 var code = (params.get("code"));
 let next = '';
+let numOfLoads = 0;
 
 fetch('https://api.spotify.com/v1/me/playlists', {
   headers:
@@ -23,6 +24,9 @@ fetch('https://api.spotify.com/v1/me/playlists', {
   .then(data => {
     console.log(data);
     next = data.next;
+
+    numOfLoads = Math.floor(data.total / 20); //determine number of times new playlists can be loaded
+    console.log("Load equal to: " + numOfLoads);
 
     for (const playlist of data.items) {
       //create HTML elements
@@ -63,59 +67,65 @@ fetch('https://api.spotify.com/v1/me/playlists', {
   .catch(console.error);
 
 function loadMore() {
-  fetch(next, {
-    headers:
-    {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + code
-    }
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      next = data.next;
-      if (next == null) {
-        loadBtn.setAttribute("visibility", "none");
-      }
-
-      for (const playlist of data.items) {
-        //create HTML elements
-        const card = document.createElement("div");
-        const grid = document.createElement("div");
-        const h3 = document.createElement("h3");
-        const h4 = document.createElement("h4");
-        const img = document.createElement("img");
-
-        //set classes
-        card.setAttribute("class", "card");
-        grid.setAttribute("class", "card-grid");
-        h3.setAttribute("class", "card-title");
-        h4.setAttribute("class", "card-owner");
-        img.setAttribute("class", "card-img");
-
-        //set text and images
-        h3.textContent = `${playlist.name}`;
-        h4.textContent = `by ${playlist.owner.display_name}`;
-        img.src = playlist.images[0].url;
-
-        //append elements to page
-        container.appendChild(card);
-        card.appendChild(img);
-        card.appendChild(grid);
-        grid.appendChild(h3);
-        grid.appendChild(h4);
-
-        //event for clicking card
-        card.addEventListener("click", () => {
-          const a = document.createElement("a");
-          a.href = "generator_mock.html";
-          a.click();
-          document.body.removeChild(a);
-        })
+  numOfLoads = numOfLoads--;
+  console.log("Load decremented to: " + numOfLoads);
+  if (numOfLoads < 0) {
+    loadBtn.setAttribute("visibility", "none");
+  } else {
+    fetch(next, {
+      headers:
+      {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + code
       }
     })
-    .catch(console.error);
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        next = data.next;
+        // if (next == null) {
+        //   loadBtn.setAttribute("visibility", "none");
+        // }
+
+        for (const playlist of data.items) {
+          //create HTML elements
+          const card = document.createElement("div");
+          const grid = document.createElement("div");
+          const h3 = document.createElement("h3");
+          const h4 = document.createElement("h4");
+          const img = document.createElement("img");
+
+          //set classes
+          card.setAttribute("class", "card");
+          grid.setAttribute("class", "card-grid");
+          h3.setAttribute("class", "card-title");
+          h4.setAttribute("class", "card-owner");
+          img.setAttribute("class", "card-img");
+
+          //set text and images
+          h3.textContent = `${playlist.name}`;
+          h4.textContent = `by ${playlist.owner.display_name}`;
+          img.src = playlist.images[0].url;
+
+          //append elements to page
+          container.appendChild(card);
+          card.appendChild(img);
+          card.appendChild(grid);
+          grid.appendChild(h3);
+          grid.appendChild(h4);
+
+          //event for clicking card
+          card.addEventListener("click", () => {
+            const a = document.createElement("a");
+            a.href = "generator_mock.html";
+            a.click();
+            document.body.removeChild(a);
+          })
+        }
+      })
+      .catch(console.error);
+  }
 }
 
 
